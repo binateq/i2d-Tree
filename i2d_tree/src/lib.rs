93e41dae1451dd4aa9c51f99b80eb11a715c4cca
Mark Shevchenko <markshevchenko@gmail.com>
2021-04-14 16:41:24 +0300

    Drop tests.rs

--- i2d_tree/src/lib.rs
+++ i2d_tree/src/lib.rs
@@ -12,10 +12,28 @@ impl Point {
     }
 }
 
+#[test]
+fn point_new_fill_fields() {
+    let point = Point::new(10.0, 200.0);
+
+    assert_eq!(point.latitude, 10.0);
+    assert_eq!(point.longitude, 200.0);
+}
+
 impl PartialEq for Point {
     fn eq(&self, other: &Self) -> bool {
         self.latitude == other.latitude && self.longitude == other.longitude
     }
+}
+
+#[test]
+fn point_eq_checks_for_equality() {
+    let point = Point::new(1.0, 20.0);
+    let same = Point::new(1.0, 20.0);
+    let different = Point::new(2.0, 10.0);
+
+    assert!(point == same);
+    assert!(point != different);
 }
 
 #[derive(Copy, Clone, Debug, PartialEq)]
@@ -28,6 +46,14 @@ impl<T: Copy> Item<T> {
     pub fn new(latitude: f64, longitude: f64, value: T) -> Item<T> {
         Item { point: Point::new(latitude, longitude), value }
     }
+}
+
+#[test]
+fn item_new_fills_fields() {
+    let item = Item::new(1.0, 20.0, "foo");
+
+    assert_eq!(item.point, Point::new(1.0, 20.0));
+    assert_eq!(item.value, "foo");
 }
 
 #[derive(Debug, PartialEq)]
